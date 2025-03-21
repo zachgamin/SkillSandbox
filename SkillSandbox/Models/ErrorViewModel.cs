41153ad964a1c4c866c4ed6b8c91aa9dc74b8f90
GoldenSilver <snpgonzales7@gmail.com>
2025-03-21 13:43:50 +0800

    Add project files.

---
+++ SkillSandbox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkillSandbox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
